31c11aa092f9de3c3ac827c96fac62588fb1712a
Abdulhalik <abdlhlkkrkmz@gmail.com>
2020-01-22 19:37:58 +0300

    Created movie model

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const index = require('./routes/index');
+const movie = require('./routes/movie');
 
 const app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/api/movie', movie);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.json({ status: 1 });
 });
 
 module.exports = router;
